bc967cf2523fd1563e3832548e4fbc9116a07150
Hank <hank.butler@protonmail.com>
2022-10-31 09:44:45 -0600

    fleshed out helloworld contract

--- udemy_course/HelloWorld.sol
+++ udemy_course/HelloWorld.sol
@@ -5,3 +5,19 @@ interface HelloWolrdInterface{
 function helloWolrd() external view returns (string memory);
 function setText(string memory newText) external;
 }
+
+contract HelloWorld is HelloWolrdInterface {
+  string private text;
+
+  constructor() {
+    text = "Hello World";
+  }
+
+  function helloWolrd() public view override returns (string memory) {
+    return text;
+  }
+
+  function setText(string memory newText) public override {
+    text = newText;
+  }
+}
