155e56872d5004b3839a8fd83f031086a4d79d29
Hank <hank.butler@protonmail.com>
2022-10-31 09:41:49 -0600

    created interface HelloWorldInterface

--- udemy_course/HelloWorld.sol
+++ udemy_course/HelloWorld.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >= 0.7.0 < 0.9.0;
 
-contract HelloWorld {
-  constructor() {}
-
-  function helloWorld() public view returns (string memory) {}
+interface HelloWolrdInterface{
+function helloWolrd() external view returns (string memory);
+function setText(string memory newText) external;
 }
