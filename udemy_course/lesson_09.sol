ec08488d940b5bb538bdee1ef048b73d8a8ad9e8
Hank <hank.butler@protonmail.com>
2022-11-07 13:35:52 -0700

    Update lesson_09.sol

--- udemy_course/lesson_09.sol
+++ udemy_course/lesson_09.sol
@@ -1,2 +1,8 @@
 // SPDX-Licence-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// Plain ERC20 reference
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK"){}
+}
