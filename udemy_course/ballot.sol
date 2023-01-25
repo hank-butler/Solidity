ae97ee48bd4db6286eb248efb1d54f92b3d6a6ce
Hank <hank.butler@protonmail.com>
2023-01-24 20:35:05 -0700

    add set and get message functions

--- udemy_course/ballot.sol
+++ udemy_course/ballot.sol
@@ -4,6 +4,14 @@ contract Inbox {
     string public message;
 
     function Inbox(string initialMessage) public {
-    
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string) {
+        return message
     }
 }
