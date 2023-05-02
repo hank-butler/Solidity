cb7de7068e5a1fc57e7a4e1352bc5942ddb8e210
Hank <hank.butler@protonmail.com>
2023-05-01 21:19:56 -0600

    Update MyContract.sol

--- udemy_course/MyContract.sol
+++ udemy_course/MyContract.sol
@@ -6,5 +6,8 @@ contract MyContract {
     function get() public view returns(string memory) {
         return value;
     }
+    function set(string memory _value) public {
+        value = _value;
+    }
 }
 
