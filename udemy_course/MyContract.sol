c2e6d6039d0feee44bda6bda43e1ddc536697d3c
Hank <hank.butler@protonmail.com>
2023-05-01 21:27:19 -0600

    added constructor

--- udemy_course/MyContract.sol
+++ udemy_course/MyContract.sol
@@ -3,11 +3,16 @@ pragma solidity 0.8.7;
 contract MyContract {
     string value; // "state variable that is stored on chain"
 
+    constructor() {
+        value = "Hello, World!";
+    }
+
+    
     function get() public view returns(string memory) {
         return value;
     }
     function set(string memory _value) public {
-        value = _value;
+        value = _value; // underscore is a convention for differentiating state and local variables
     }
 }
 
