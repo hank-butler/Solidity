aab2dc24b2f132f99328e14df1cd88a23c64e901
Hank <hank.butler@protonmail.com>
2023-05-01 21:19:07 -0600

    Update MyContract.sol

--- udemy_course/MyContract.sol
+++ udemy_course/MyContract.sol
@@ -1,5 +1,10 @@
 pragma solidity 0.8.7;
 
 contract MyContract {
-	string value; // "state variable that is stored on chain"
+    string value; // "state variable that is stored on chain"
+
+    function get() public view returns(string memory) {
+        return value;
+    }
 }
+
