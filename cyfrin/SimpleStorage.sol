32b5d5c6f12a8ed0cf4f133b7e7d05ce7f1a4ab0
Hank <hank.butler@protonmail.com>
2024-02-05 20:49:24 -0700

    functions tutorial

--- cyfrin/SimpleStorage.sol
+++ cyfrin/SimpleStorage.sol
@@ -1,8 +1,18 @@
 pragma solidity 0.8.19; // stating our version
 
 contract SimpleStorage {
-    // basic types: bool, uint, int, address, bytes
-    bool hasFavoriteNumber = true;
-    int256 favoriteNumber = 88;
-    string favoriteNumberInText = "88";
+    // favoriteNumber initialized at 0 if no value given (0 is default value for uint256)
+    uint256 public favoriteNumber; 
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+        // view => read state from blockchain
+    }
+
+    
 }
