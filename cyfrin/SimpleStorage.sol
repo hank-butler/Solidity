930921f3e87a87af8dd20ed70db6a84632d6a6b2
Hank <hank.butler@protonmail.com>
2024-02-05 18:45:45 -0700

    Update SimpleStorage.sol

--- cyfrin/SimpleStorage.sol
+++ cyfrin/SimpleStorage.sol
@@ -1,1 +1,8 @@
 pragma solidity 0.8.19; // stating our version
+
+contract SimpleStorage {
+    // basic types: bool, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    int256 favoriteNumber = 88;
+    string favoriteNumberInText = "88";
+}
