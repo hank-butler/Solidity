66349cd5d907c20fcd7b80f75fbdcfd93aee4308
Hank <hank.butler@protonmail.com>
2024-02-06 20:33:48 -0700

    arrays and structs lesson

--- cyfrin/SimpleStorage.sol
+++ cyfrin/SimpleStorage.sol
@@ -2,17 +2,33 @@ pragma solidity 0.8.19; // stating our version
 
 contract SimpleStorage {
     // favoriteNumber initialized at 0 if no value given (0 is default value for uint256)
-    uint256 public favoriteNumber; 
+    uint256 myFavoriteNumber; 
+
+    // uint256[] listOfFavoriteNumbers; // array of uint256's, [0, 78,90]
+
+    // create custom data type with struct
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    //type [] to empty array, visibility, name of array
+    // array is dynamic 
+    Person[] public listOfPeople;
+    // static array Person[3] => max 3 people in array
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
         
     }
 
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
         // view => read state from blockchain
     }
 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    }
     
 }
