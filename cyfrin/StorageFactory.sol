55cf1500d38d3773a7dd4f1c1c3ba9232c25088a
Hank <hank.butler@protonmail.com>
2024-02-08 17:48:38 -0700

    get and store functions

--- cyfrin/StorageFactory.sol
+++ cyfrin/StorageFactory.sol
@@ -13,4 +13,16 @@ contract StorageFactory {
         SimpleStorage newSimpleStorageContract = new SimpleStorage();
         listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+
+        // ABI (just need function selector)
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
 }
