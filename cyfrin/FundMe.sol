969a9600a40090f663e23a072314db470f565b64
Hank <hank.butler@protonmail.com>
2024-02-14 22:04:54 -0700

    refactoring with interface imported

--- cyfrin/FundMe.sol
+++ cyfrin/FundMe.sol
@@ -6,54 +6,56 @@
 
 pragma solidity ^0.8.18;
 
-interface AggregatorV3Interface {
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-  function decimals()
-    external
-    view
-    returns (
-      uint8
-    );
+// interface AggregatorV3Interface {
 
-  function description()
-    external
-    view
-    returns (
-      string memory
-    );
+//   function decimals()
+//     external
+//     view
+//     returns (
+//       uint8
+//     );
 
-  function version()
-    external
-    view
-    returns (
-      uint256
-    );
+//   function description()
+//     external
+//     view
+//     returns (
+//       string memory
+//     );
 
-  function getRoundData(
-    uint80 _roundId
-  )
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+//   function version()
+//     external
+//     view
+//     returns (
+//       uint256
+//     );
 
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
+//   function getRoundData(
+//     uint80 _roundId
+//   )
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
 
-}
+//   function latestRoundData()
+//     external
+//     view
+//     returns (
+//       uint80 roundId,
+//       int256 answer,
+//       uint256 startedAt,
+//       uint256 updatedAt,
+//       uint80 answeredInRound
+//     );
+
+// }
 
 contract FundMe {
 
