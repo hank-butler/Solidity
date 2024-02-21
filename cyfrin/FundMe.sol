af9c540752fa1e0dd9278119b09462622530686b
Hank <hank.butler@protonmail.com>
2024-02-20 20:48:34 -0700

    commented out and refactored code moved to library

--- cyfrin/FundMe.sol
+++ cyfrin/FundMe.sol
@@ -6,9 +6,10 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5e18;
 
@@ -20,8 +21,10 @@ contract FundMe {
         // payable makes function accept eth
         // makes contract function like a wallet
         // msg.value is global
-        require(getConversionRate(msg.value) >= minimumUsd, "Insufficient funds");
+        // First input variable is type you're importing from library
+        require(msg.value.getConversionRate() >= minimumUsd, "Insufficient funds");
         // reverts if require condition not met (returns variable back to original state)
+        
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
@@ -31,25 +34,25 @@ contract FundMe {
 
     // function withdraw() public {}
 
-    function getPrice() public view returns(uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    // function getPrice() public view returns(uint256){
+    //     // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        // Price of ETH in terms of USD
-        return uint256(price * 1e10);
-    }
+    //     // ABI
+    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    //     (, int256 price,,,) = priceFeed.latestRoundData();
+    //     // Price of ETH in terms of USD
+    //     return uint256(price * 1e10);
+    // }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        // multiply before dividing because of whole nums in solidity
-        // will make sense in foundry section
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
+    // function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    //     uint256 ethPrice = getPrice();
+    //     // multiply before dividing because of whole nums in solidity
+    //     // will make sense in foundry section
+    //     uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+    //     return ethAmountInUsd;
+    // }
 
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+    // function getVersion() public view returns(uint256){
+    //     return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    // }
 }
